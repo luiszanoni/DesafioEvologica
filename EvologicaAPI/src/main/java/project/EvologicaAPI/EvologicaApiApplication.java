847a82e0c798b4055095ba1ebc9e1ce38d5fe7a2
lgzanoni <luiszanoni01@gmail.com>
2025-04-07 23:30:56 -0300

    Commit Inicial de API Spring

---
+++ EvologicaAPI/src/main/java/project/EvologicaAPI/EvologicaApiApplication.java
@@ -0,0 +1,13 @@
+package project.EvologicaAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvologicaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvologicaApiApplication.class, args);
+	}
+
+}
